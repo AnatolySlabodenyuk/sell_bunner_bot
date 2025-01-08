bdb072844b5ab0d4bd60f8ec3aab533bcd2ad0ef
slabodenyuk <slabodenyukanatoly@gmail.com>
2025-01-08 23:18:53 +0700

    add new inline kb

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -1,6 +1,8 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from database.create_base import get_name_from_table
+from database.example_table import get_name_from_item_table
+from database.tables import get_banner_size_and_price_from_products_table, get_banner_name_from_products_table
+from lexicon.products_enum import ProductsEnum
 from aiogram.filters.callback_data import CallbackData
 
 
@@ -8,8 +10,13 @@ class ProductsCallbackFactory(CallbackData, prefix="products"):
     product_name: str
 
 
-class BannerTypesCallbackFactory(CallbackData, prefix="banner"):
+class BannerTypesCallbackFactory(CallbackData, prefix="banner_type"):
     bunner_type: str | None
+    is_back: bool
+
+
+class BannerSizeCallbackFactory(CallbackData, prefix="banner_size_price"):
+    products_name: str | None
     is_back: bool
 
 
@@ -18,33 +25,53 @@ class TarpaulinDensitiesCallbackFactory(CallbackData, prefix="tarpaulin"):
     is_back: bool
 
 
-async def create_products_inline_kb(width: int) -> InlineKeyboardMarkup:
+# async def create_products_inline_kb(width: int) -> InlineKeyboardMarkup:
+#     """
+#     Функция для формирования инлайн-клавиатуры с продукцией из базы
+#     """
+#     products_kb_builder = InlineKeyboardBuilder()
+#     buttons_list: list[InlineKeyboardButton] = []
+
+#     product_name_list = await get_name_from_table()
+
+#     for product_name in product_name_list:
+#         buttons_list.append(
+#             InlineKeyboardButton(
+#                 text=product_name,
+#                 callback_data=ProductsCallbackFactory(
+#                     product_name=product_name
+#                 ).pack()
+#             )
+#         )
+
+#     products_kb_builder.row(*buttons_list, width=width)
+
+#     return products_kb_builder.as_markup()
+
+
+async def create_products_inline_kb() -> InlineKeyboardMarkup:
     """
-    Функция для формирования инлайн-клавиатуры с продукцией "на лету"
+    Функция для формирования инлайн-клавиатуры с продукцией
     """
     products_kb_builder = InlineKeyboardBuilder()
-    buttons_list: list[InlineKeyboardButton] = []
+    products_list = [product.value for product in ProductsEnum]
 
-    product_name_list = await get_name_from_table()
-
-    for product_name in product_name_list:
-        buttons_list.append(
-            InlineKeyboardButton(
-                text=product_name,
-                callback_data=ProductsCallbackFactory(
-                    product_name=product_name
-                ).pack()
+    for product in products_list:
+        products_kb_builder.button(
+            text=product,
+            callback_data=ProductsCallbackFactory(
+                product_name=product
             )
         )
 
-    products_kb_builder.row(*buttons_list, width=width)
+    products_kb_builder.adjust(len(products_list) - 1)
 
     return products_kb_builder.as_markup()
 
 
 async def create_bunner_types_inline_kb() -> InlineKeyboardMarkup:
     """
-    Функция для формирования инлайн-клавиатуры с продукцией "на лету"
+    Функция для формирования инлайн-клавиатуры с типом баннеров
     """
     banner_kp_builder = InlineKeyboardBuilder()
     bunner_type_list = ['Китай', 'Европа']
@@ -64,14 +91,61 @@ async def create_bunner_types_inline_kb() -> InlineKeyboardMarkup:
             is_back=True
         )
     )
-    banner_kp_builder.adjust(2)
+    banner_kp_builder.adjust(len(bunner_type_list))
 
     return banner_kp_builder.as_markup()
 
 
+async def create_bunner_size_inline_kb(banner_type: str) -> InlineKeyboardMarkup:
+    """
+    Функция для формирования инлайн-клавиатуры с ценой на баннера из базы
+    """
+    bunner_size_kb_builder = InlineKeyboardBuilder()
+    buttons_list: list[InlineKeyboardButton] = []
+
+    banner_names_list = await get_banner_name_from_products_table(banner_type)
+
+    # for bunner_size_data in bunner_size_data_list:
+    #     buttons_list.append(
+    #         InlineKeyboardButton(
+    #             text=f'Размер {bunner_size_data[0]
+    #                            } - {bunner_size_data[1]} р.',
+    #             callback_data=BannerSizePriceCallbackFactory(
+    #                 banner_size=bunner_size_data[0],
+    #                 is_back=False
+    #             ).pack()
+    #         )
+    #     )
+
+    for products_name in banner_names_list:
+        buttons_list.append(
+            InlineKeyboardButton(
+                text=f'{products_name}',
+                callback_data=BannerSizeCallbackFactory(
+                    products_name=products_name,
+                    is_back=False
+                ).pack()
+            )
+        )
+
+    buttons_list.append(
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data=BannerSizeCallbackFactory(
+                products_name=None,
+                is_back=True
+            ).pack()
+        )
+    )
+
+    bunner_size_kb_builder.row(*buttons_list, width=1)
+
+    return bunner_size_kb_builder.as_markup()
+
+
 async def create_tarpaulin_densities_inline_kb() -> InlineKeyboardMarkup:
     """
-    Функция для формирования инлайн-клавиатуры с плотностью тарпаулина "на лету"
+    Функция для формирования инлайн-клавиатуры с плотностью тарпаулина
     """
     tarpaulin_densities_kb_builder = InlineKeyboardBuilder()
     density_list = [60, 90, 120, 150, 180, 220, 280]
